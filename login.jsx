37e06c46453db0c3724642eb9b5d2bc43ff4a948
Pankaja Samaranayaka <125545343+Pankaja04@users.noreply.github.com>
2023-07-24 10:07:10 +0530

    front end
    front end

---
+++ login.jsx
@@ -0,0 +1,91 @@
+import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import "./login.css"
+
+const Login = () => {
+
+    const [user,setUser] = useState({
+        email : '',
+        password : ''
+    });
+
+    //Handle Input
+    const handleChange = (event) =>{
+        let name = event.target.name
+        let value = event.target.value
+
+        setUser({...user, [name]:value})
+    }
+
+    //Hadle Login
+    const handleSubmit = async(event)=>{
+        event.preventDefault();
+        const{email,password} = user;
+        try {
+            const res = await fetch('/login',{
+                method : "POST",
+                headers : {
+                    "Content-Type" : "application/json"
+                },
+                body : JSON.stringify({
+                    email,password
+                })
+            });
+
+            if(res.status === 400 || !res){
+                window.alert("Invalid Credentials");
+            }else{
+                window.alert("Login Successfull");
+                window.location.replace("/");
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    return (
+        <div><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+            <div className="container shadow my-5">
+                <div className="row">
+                    <div className="col-md-5 d-flex flex-column align-items-center text-white justify-content-center form">
+                        <h1 className="display-4 fw-bolder">Welcome Back</h1>
+                        <p className="lead text-center">Enter Your Credentials To Login</p>
+                        <h5 className="mb-4">OR</h5>
+                        <Link to="/Register" className="btn btn-outline-light rounded-pill pb-2 w-50">Register</Link>
+
+                        {/* <i className={d.icon6 } ></i>
+                  <Link to="/ChromeExtension" className="page-scroll">
+                  <h3>{d.name6}</h3></Link> */}
+                    {/* <p>{d.text6}</p> */}
+                    </div>
+                    <div className="col-md-6 p-5">
+                        <h1 className="display-6 fw-bolder mb-5">LOGIN</h1>
+                        <form onSubmit={handleSubmit}>
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                                name="email"
+                                value = {user.email} 
+                                onChange={handleChange}/>
+                                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Password</label>
+                                <input type="password" class="form-control" id="exampleInputPassword1" 
+                                 name="password"
+                                 value = {user.password} 
+                                 onChange={handleChange}/>
+                            </div>
+                            <div class="mb-3 form-check">
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+                                <label class="form-check-label" for="exampleCheck1">Remember me</label>
+                            </div>
+                            <button type="submit" class="btn btn-primary w-100 mt-4 rounded-pill">Login</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
---
+++ singin.jsx
@@ -0,0 +1,98 @@
+import { NavLink } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+
+
+const Register = () => {
+    const history = useNavigate();
+    const [user, setUser] = useState({
+        username: "",
+        email: "",
+        password: ""
+    });
+
+    // Handle Inputs
+    const handleInput = (event) => {
+        let name = event.target.name;
+        let value = event.target.value;
+
+        setUser({ ...user, [name]: value });
+    };
+
+    // Handle Submit
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        // Object Destructuring
+        // Store Object data into variables
+        const { username, email, password } = user;
+        try {
+            const res = await fetch('/register', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    username, email, password
+                })
+            });
+            console.log(res.status);
+            if (res.status === 400 || !res) {
+                window.alert("Already Used Details");
+            } else {
+                window.alert("Registered Successfully");
+                history.push('/login');
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return (
+        <div><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+            <div className="container shadow my-5">
+                <div className="row justify-content-end">
+                    <div className="col-md-5 d-flex flex-column align-items-center text-white justify-content-center custom-form order-2">
+                        <h1 className="display-4 fw-bolder">Hello, Friend</h1>
+                        <p className="lead text-center">Enter Your Details To Register</p>
+                        <h5 className="mb-4">OR</h5>
+                        <NavLink to="/login" className="btn btn-outline-light rounded-pill pb-2 w-50">Login</NavLink>
+                    </div>
+                    <div className="col-md-6 p-5">
+                        <form onSubmit={handleSubmit} method="POST">
+                            <div className="mb-3">
+                                <label htmlFor="name" className="form-label">Username</label>
+                                <input type="text" className="form-control" id="name"
+                                    name="username"
+                                    value={user.username}
+                                    onChange={handleInput} />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                                    name="email"
+                                    value={user.email}
+                                    onChange={handleInput} />
+                                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                                <input type="password" className="form-control" id="exampleInputPassword1"
+                                    name="password"
+                                    value={user.password}
+                                    onChange={handleInput} />
+                            </div>
+                            <div className="mb-3 form-check">
+                                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                                <label className="form-check-label" htmlFor="exampleCheck1">I Agree Terms and Conditions</label>
+                            </div>
+                            <button type="submit" className="btn btn-outline-dark w-100 mt-4 rounded-pill">Register</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
