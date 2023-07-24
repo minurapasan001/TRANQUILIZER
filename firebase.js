ab72682cc6e360e4549243ce4a1c1ada040921a9
Minura Pasan <105039109+minurapasan001@users.noreply.github.com>
2023-07-24 10:16:24 +0530

    back end
    back end

---
+++ firebase.js
@@ -0,0 +1,23 @@
+// // firebase.js
+
+// import { initializeApp } from 'firebase/app';
+// import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {apiKey: "AIzaSyBwWjEHQOMtmM3iivF4TBcfz06PzttlzKQ",
+// authDomain: "sdgp-sing-in.firebaseapp.com",
+// projectId: "sdgp-sing-in",
+// storageBucket: "sdgp-sing-in.appspot.com",
+// messagingSenderId: "625835474945",
+// appId: "1:625835474945:web:9fa38ae2199b0cd0ee8000"
+// };
+//   // Your Firebase configuration object here
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// // Initialize Firebase Authentication and get a reference to the service
+// const auth = getAuth(app);
+
+// export { auth, signInWithEmailAndPassword };
+// export default app;
